62ad2747ed13659e75759e995f124e72ed2cc215
Vlad <rommel66699@gmail.com>
2018-12-14 11:34:15 +0200

    new nav panel

--- index.php
+++ index.php
@@ -14,27 +14,55 @@
     <head>
     <link href="css/style.css" type="text/css" rel="stylesheet">
     <link href="css/normalize.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Exo:300" rel="stylesheet">
     </head>
     <body>
         <header>
             <nav class="main-nav">
                 <ul class="main-nav main-nav-list">
-                    <li class="main-nav-list main-nav-item"><a href="#">Logo</a></li>
+                    <li class="main-nav-list main-nav-item  main-nav-item-logo"><a href="#">Logo</a></li>
                     <li class="main-nav-list main-nav-item"><a href="#">Main</a></li>
                     <li class="main-nav-list main-nav-item"><a href="#">Movies</a></li>
                     <li class="main-nav-list main-nav-item"><a href="#">Series</a></li>
                     <li class="main-nav-list main-nav-item"><a href="#">Forum</a></li>
-                    <li class="main-nav-list main-nav-item"><a href="#">Sign in</a></li>
+                </ul>
+                <ul class="user-sector">
+                    <li class="user-sector-item">
+                        <button class="login-button">
+                        <img class="user-log-icon" src="interface_pics/login-square-arrow-button-outline.svg"></button>
+                    </li>
                 </ul>
             </nav>
         </header>
         <main>
-            <section></section>
+            <section class="news-section">
+                <div class="article">
+                    <img class="promo-pic" src="#">
+                    <h3 class="article-title">Title</h3>
+                    <p class="article-text">Text text text text</p>
+                    <a href="#">Read more...</a>
+                </div>
+            </section>
+
+            <section class="contents-section">
+                <a class="movie-url" href="#"><img src="#"></a>
+            </section>
 
         </main>
 
         <footer>
-
+            <div class="contact-adress">
+                <p class="adress-content">
+                    You contact to us with Email below.
+                </p>
+            </div>
+            <div class="copyright">
+                <p class="copy-content">
+                    Made by Neo
+                <div>Icons made by <a href="https://www.flaticon.com/authors/catalin-fertu" title="Catalin Fertu">Catalin Fertu</a> from <a href="https://www.flaticon.com/"
+                     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"  title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                </p>
+            </div>
         </footer>
     </body>
 </html>
