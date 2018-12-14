326ff1bca6c2312cb46d10db6f80c6f05f3fa732
Vlad <rommel66699@gmail.com>
2018-12-14 07:15:47 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Predator
+ * Date: 10/21/18
+ * Time: 18:35
+ */
+
+
+?>
+
+<!DOCTYPE>
+<html lang="EN">
+    <head>
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <link href="css/normalize.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <nav class="main-nav">
+                <ul class="main-nav main-nav-list">
+                    <li class="main-nav-list main-nav-item"><a href="#">Logo</a></li>
+                    <li class="main-nav-list main-nav-item"><a href="#">Main</a></li>
+                    <li class="main-nav-list main-nav-item"><a href="#">Movies</a></li>
+                    <li class="main-nav-list main-nav-item"><a href="#">Series</a></li>
+                    <li class="main-nav-list main-nav-item"><a href="#">Forum</a></li>
+                    <li class="main-nav-list main-nav-item"><a href="#">Sign in</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section></section>
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
---
+++ start_page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Predator
+ * Date: 11/5/18
+ * Time: 2:36
+ */
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+
+    </head>
+    <body>
+
+    </body>
+</html>
